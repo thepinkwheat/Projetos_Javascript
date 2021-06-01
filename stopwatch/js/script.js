5d4ba869fdbb51816ed4aa238251a128e23c4035
thepinkwheat <37757146+thepinkwheat@users.noreply.github.com>
2021-06-01 11:13:28 -0300

    Formatação 00-00-00
    Formatação para o relógio manter-se no formato do css

--- stopwatch/js/script.js
+++ stopwatch/js/script.js
@@ -9,28 +9,40 @@ start_id.addEventListener('click', startCount)
 function startCount() {
     click++
     if(click == 1){
+        timer_form.innerHTML = '00:00:00'
+         
+        if(minute<10 || minute==0){
+            minute = '0'+minute
+        }
+         
+        if(hour<10||hour==0){
+            hour = '0'+hour
+        } 
         timeCycle()
     }
 }
 
 function timeCycle(){
 
-    timer_form.innerHTML = ''
-    second = second + 1
+    
+    timer_form.innerHTML = '00:00:00'
+    second++
+
+    if(second<10 || second==0){
+        second = '0'+second
+    }
 
     if(second == 60){
-        minute = minute + 1
+        minute++
         second = 0
     }
 
     if(minute == 60){
-        hour = hour + 1
+        hour++
         minute = 0
     }
 
     timer_form.innerHTML = `${hour}:${minute}:${second}`
-    
-    
-    setTimeout("timeCycle()", 1000)
     console.log(`segundo:${second}, minuto:${minute}, hora:${hour}`)
+    setTimeout("timeCycle()", 1000)
 }

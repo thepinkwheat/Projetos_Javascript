7a817caf5fe3894910a0407388096bfc7bf7772f
thepinkwheat <37757146+thepinkwheat@users.noreply.github.com>
2021-06-02 02:37:17 -0300

    Tirei o formato 00:00:00
    O formato estava instável, resolvi retirar e procurar o motivo da instabilidade e tentar arrumar na versão 2.0

--- stopwatch/js/script.js
+++ stopwatch/js/script.js
@@ -1,6 +1,6 @@
 const start_id = window.document.querySelector('#start')
 const timer_form = window.document.querySelector('#timer_id')
-var second = 59
+var second = 0
 var hour = 0
 var minute = 0
 let click = 0
@@ -9,15 +9,6 @@ start_id.addEventListener('click', startCount)
 function startCount() {
     click++
     if(click == 1){
-        timer_form.innerHTML = '00:00:00'
-         
-        if(minute<10 || minute==0){
-            minute = '0'+minute
-        }
-         
-        if(hour<10||hour==0){
-            hour = '0'+hour
-        } 
         timeCycle()
     }
 }
@@ -27,10 +18,6 @@ function timeCycle(){
     
     timer_form.innerHTML = '00:00:00'
     second++
-
-    if(second<10 || second==0){
-        second = '0'+second
-    }
 
     if(second == 60){
         minute++

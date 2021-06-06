436aeb650e538f2fa1a51aaebd29a61ac431ecd4
thepinkwheat <37757146+thepinkwheat@users.noreply.github.com>
2021-06-06 05:31:21 -0300

    Todo-list finalizado

--- todo-list/js/script.js
+++ todo-list/js/script.js
@@ -1,6 +1,7 @@
 const btn_createItem = window.document.getElementById('create-item')
 const itemList = window.document.getElementById('list')
 let itemValue = window.document.getElementById('itemValue')
+let btnDelete = window.document.getElementsByClassName('delete')
 
 btn_createItem.addEventListener('click', createItem)
 
@@ -10,10 +11,8 @@ function createItem(){
     let textItem = document.createTextNode(itemValue.value)
 
     btnItem.innerText = 'Excluir'    
-    btnItem.className = 'btn'
+    btnItem.className = 'delete'
     item.className = "item-list"
-
-    
     
     item.appendChild(btnItem)
     item.appendChild(textItem)
@@ -21,4 +20,23 @@ function createItem(){
     console.log(item)
 
     itemList.appendChild(item)
+    buttonDeleteFunction()
 }
+
+function deleteItem(){
+    this.parentElement.remove() //this representa o botão
+    console.log(this)
+}
+
+function buttonDeleteFunction(){
+    for (let i = 0; i <= btnDelete.length; i++) {
+
+        btnDelete[i].addEventListener("click", deleteItem)
+        
+    }
+}
+
+//this é usado quando a função é criada para trabalhar 
+//com um valor especificado em outro local
+//, nesse caso, chamamos a função para trabalhar 
+//com o valor do botão dentro do laço de repetição.
